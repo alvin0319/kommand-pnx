61b42df339aafb061ff062f72da8832021d0f790
alvin0319 <alvin7810@naver.com>
2023-06-14 23:28:19 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,79 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    `maven-publish`
+}
+
+val libraryVersion = "1.0-SNAPSHOT"
+
+group = "dev.minjae.pnx.kommand"
+version = libraryVersion
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
+    maven {
+        name = "opencollab-repo-release"
+        url = uri("https://repo.opencollab.dev/maven-releases")
+    }
+    maven {
+        name = "opencollab-repo-snapshot"
+        url = uri("https://repo.opencollab.dev/maven-snapshots")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    api("cn.powernukkitx:powernukkitx:1.20.0-r1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+val javadoc: Javadoc by tasks
+
+val sourcesJar = task<Jar>("sourcesJar") {
+    from(sourceSets["main"].allSource)
+    archiveClassifier.set("sources")
+}
+
+val javadocJar = task<Jar>("javadocJar") {
+    from(javadoc.destinationDir)
+    archiveClassifier.set("javadoc")
+
+    dependsOn(javadoc)
+}
+
+tasks {
+    build {
+        dependsOn(javadocJar)
+        dependsOn(sourcesJar)
+        dependsOn(jar)
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
+
+kotlin {
+    jvmToolchain(19)
+}
+
+// setup maven central publishing
+
+publishing.publications {
+    register<MavenPublication>("Release") {
+        from(components["java"])
+        groupId = project.group as String
+        artifactId = project.name
+        version = project.version as String
+
+        artifact(javadocJar)
+        artifact(sourcesJar)
+    }
+}
---
+++ src/main/kotlin/dev/minjae/pnx/kommand/kommand.kt
@@ -0,0 +1,110 @@
+package dev.minjae.pnx.kommand // ktlint-disable filename
+
+import cn.nukkit.command.CommandSender
+import cn.nukkit.command.PluginCommand
+import cn.nukkit.command.data.CommandParamType
+import cn.nukkit.command.data.CommandParameter
+import cn.nukkit.command.tree.ParamList
+import cn.nukkit.command.tree.node.IParamNode
+import cn.nukkit.command.utils.CommandLogger
+import cn.nukkit.plugin.PluginBase
+
+typealias KommandDefaultHandler = (CommandSender, String?, Array<out String>) -> Boolean
+typealias KommandParamHandler = (CommandSender, String?, MutableMap.MutableEntry<String, ParamList>, CommandLogger) -> Int
+
+/**
+ * Creates new [Kommand] object and returns it.
+ * @param name The name of the command.
+ * @param description The description of the command.
+ * @return The created [Kommand] object.
+ */
+inline fun <reified T : PluginBase> T.command(name: String, description: String, block: Kommand.() -> Unit): Kommand {
+    return Kommand(name, description, this).apply {
+        block(this)
+    }
+}
+
+/**
+ * Adds overload to the [Kommand].
+ * @param key The key of the overload.
+ */
+inline fun <reified T : Kommand> T.overload(key: String, block: KommandOverload.() -> Unit) {
+    val overload = KommandOverload()
+    block(overload)
+    addCommandParameters(key, overload.params.toTypedArray())
+}
+
+/**
+ * Adds [CommandParameter] to the [Kommand].
+ * @param name The name of the parameter.
+ * @param description The description of the parameter.
+ * @param type The type of the parameter.
+ */
+inline fun <reified T : Kommand> T.param(
+    name: String,
+    type: CommandParamType,
+    node: IParamNode<Any>? = null
+) {
+    addCommandParameters(name, arrayOf(CommandParameter.newType(name, type, node)))
+}
+
+/**
+ * Adds [CommandParameter] to the [KommandOverload].
+ * @param name The name of the parameter.
+ * @param description The description of the parameter.
+ */
+inline fun <reified T : KommandOverload> T.param(
+    name: String,
+    type: CommandParamType,
+    node: IParamNode<Any>? = null
+) {
+    params.add(CommandParameter.newType(name, type, node))
+}
+
+inline fun <reified T : Kommand> T.onExecute(noinline execute: KommandDefaultHandler) {
+    if (paramHandler != null) {
+        throw IllegalStateException("Cannot set default handler when param handler is set")
+    }
+    defaultHandler = execute
+}
+
+inline fun <reified T : Kommand> T.onExecute(noinline execute: KommandParamHandler) {
+    if (defaultHandler != null) {
+        throw IllegalStateException("Cannot set param handler when default handler is set")
+    }
+    paramHandler = execute
+}
+
+open class Kommand(
+    name: String,
+    description: String,
+    plugin: PluginBase
+) : PluginCommand<PluginBase>(name, description, plugin) {
+
+    var defaultHandler: KommandDefaultHandler? = null
+
+    var paramHandler: KommandParamHandler? = null
+        set(value) {
+            field = value
+            if (!hasParamTree()) {
+                enableParamTree()
+            }
+        }
+
+    override fun execute(sender: CommandSender, commandLabel: String?, args: Array<out String>): Boolean {
+        return defaultHandler?.invoke(sender, commandLabel, args) ?: false
+    }
+
+    override fun execute(
+        sender: CommandSender,
+        commandLabel: String?,
+        result: MutableMap.MutableEntry<String, ParamList>,
+        log: CommandLogger
+    ): Int {
+        return paramHandler?.invoke(sender, commandLabel, result, log) ?: 0
+    }
+}
+
+open class KommandOverload {
+    val params: MutableList<CommandParameter> = mutableListOf()
+}
